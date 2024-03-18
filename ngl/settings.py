66c413ef8f3371c1e447d6cba389f4e2da53a942
Bhavesh Kumar <bhavesh@Bhaveshs-MacBook-Pro.local>
2024-03-17 22:59:40 -0700

    initial commit

--- ngl/settings.py
+++ ngl/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4exhin%s$4l1$((2oox)jwt4j++*9!l&v6ak9elgum7)#-k41u'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
